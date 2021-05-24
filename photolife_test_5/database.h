341a37be68f81425f9cfddf95732e0f3faea056d
ZadnepryanskayaAlicia <71925051+ZadnepryanskayaAlicia@users.noreply.github.com>
2021-05-24 13:41:54 +0300

    Add files via upload

---
+++ photolife_test_5/database.h
@@ -0,0 +1,47 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <QObject>
+#include <QSql>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QSqlDatabase>
+#include <QFile>
+#include <QDate>
+#include <QDebug>
+#include <QVariant>
+
+// Директивы имен таблицы, полей таблицы и базы данных
+#define DATABASE_HOSTNAME   "photolifeDataBase"
+#define DATABASE_NAME       "photolife.db"
+#define TABLE                   "images"
+
+class DataBase : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit DataBase(QObject *parent = 0);
+    ~DataBase();
+    /* Методы для непосредственной работы с классом
+     * Подключение к базе данных и вставка записей в таблицу
+     * */
+    void connectToDataBase();
+    bool openDataBase();
+    bool closeDataBase();
+
+
+private:
+    // Сам объект базы данных, с которым будет производиться работа
+    QSqlDatabase    db;
+
+private:
+    /* Внутренние методы для работы с базой данных
+     * */
+   // bool openDataBase();
+    bool restoreDataBase();
+   // bool closeDataBase();
+    bool createTable();
+};
+
+#endif // DATABASE_H
