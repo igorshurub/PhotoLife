f15dee13ae304d422ff1cbed4bbee8a817947beb
ZadnepryanskayaAlicia <71925051+ZadnepryanskayaAlicia@users.noreply.github.com>
2021-05-27 17:39:02 +0300

    Add files via upload

---
+++ photolife_test_5/database.cpp
@@ -0,0 +1,48 @@
+#include "database.h"
+
+DataBase::DataBase(QObject *parent) : QObject(parent)
+{
+}
+
+DataBase::~DataBase()
+{
+}
+
+/* Функция для подключения к базе данных
+ * */
+void DataBase::connectToDataBase()
+{
+    if(QFile("C:/Qt/photolife/" DATABASE_NAME).exists())
+        this->openDataBase();
+}
+
+/* Функция для открытия базы данных
+ * */
+bool DataBase::openDataBase()
+{
+     db=QSqlDatabase::addDatabase("QSQLITE");
+     db.setHostName(DATABASE_HOSTNAME);
+     db.setDatabaseName("C:/Qt/photolife/"  DATABASE_NAME);
+     if(db.open()){
+         return 1;
+     } else {
+         return 0;
+     }
+ }
+
+/* Функция для закрытия базы данных
+ * */
+
+bool DataBase::closeDataBase()
+{
+    db=QSqlDatabase::addDatabase("QSQLITE");
+    db.setHostName(DATABASE_HOSTNAME);
+    db.setDatabaseName("C:/Qt/photolife/"  DATABASE_NAME);
+   // db.open();
+    if(db.open()){
+        db.close();
+        return 1;
+    } else {
+        return 0;
+    }
+}
